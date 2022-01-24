552ad011d17db4db7231711083bb12645741deb4
AnishaSloes <anishasloes@gmail.com>
2022-01-24 10:02:46 +0100

    Implementation breadth first algorithm

---
+++ code/algorithms/breadth_first.py
@@ -0,0 +1,21 @@
+class BreadthFirst():
+    """
+    This algorithm builds a queue of possible state of the game.
+
+    Only unique states are in the queue (no repetitions of states)
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def get_next_state(self):
+        pass
+
+    def build_children(self):
+        pass
+
+    def check_solution(self):
+        pass
+
+    def run(self):
+        pass
