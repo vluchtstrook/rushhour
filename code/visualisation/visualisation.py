fda9e339810d9b170ebaede27b82a245d60ca117
Julius <juliuskempen13@gmail.com>
2022-01-25 17:33:54 +0100

    visualisation changed

--- code/visualisation/visualisation.py
+++ code/visualisation/visualisation.py
@@ -2,14 +2,15 @@
 def visualisation(solution, grid_size):
 
     # Go trough every state/move in the archive
-    for i in range(len(solution.states)):
+    for i in range(len(solution.path)):
 
         print('------------')
 
-        for j in range(0, len(solution.states[i]), grid_size):
+        for j in range(0, len(solution.path[i]), grid_size):
             for k in range(grid_size):
-                print(solution.states[i][j + k], end = ' ')
+                print(solution.path[i][j + k], end = ' ')
             print()
-        print('\n')
+        print()
 
-    print(f'It took {solution.count} steps.', end = '\n\n')
+    print(f'The solutions path length is {len(solution.path)}.')
+    print(f'It took creating {solution.count_states} states, of which {solution.count_unique_states} unique.', end='\n\n')
