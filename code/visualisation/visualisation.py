b1ab20dbf3cd27006f796b1e795ec7e92d3da4e0
tjardaleppers <t.leppers@outlook.com>
2022-01-25 16:05:26 +0100

    Changed while loop to forloop in visualisation

--- code/visualisation/visualisation.py
+++ code/visualisation/visualisation.py
@@ -1,20 +1,16 @@
 
-
 def visualisation(solution, grid_size):
 
     # Go trough every state/move in the archive
-    for i in range(len(solution.path)):
+    for i in range(len(solution.states)):
 
-        # print(solution.moves_made[i])
+        print(solution.moves_made[i])
         print('------------')
 
-        j = 0
-        while j < len(solution.path[i]):
+        for j in range(0, len(solution.states[i]), grid_size):
             for k in range(grid_size):
-                print(solution.path[i][j + k], end = ' ')
+                print(solution.states[i][j + k], end = ' ')
             print()
-            j += grid_size
-        print()
+        print('\n')
 
-    print(f'The solutions path length is {len(solution.path)}.')
-    print(f'It took creating {solution.count_states} states, of which {solution.count_unique_states} unique.', end = '\n\n')
+    print(f'It took {solution.count} steps.', end = '\n\n')
