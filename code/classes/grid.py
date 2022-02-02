6dc8153bd3fa39ba0dfd06e2940a573b1c372c94
tjardaleppers <t.leppers@outlook.com>
2022-02-02 11:45:12 +0100

    Cleaned up comments/description of solution.py and grid.py

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -6,7 +6,7 @@ Team: vluchtstrook
 
 This file contains a class named Grid. It defines two methods that (1) calculates the 'exit' of the grid and checks if 
 the red car (or car 'X') of the grid is in the exit spot and (2) checks if a specific move is legal.
-An instance of the Rushhour class is created in the loader file (loader.py).
+An instance of the Grid class is created in the loader file (loader.py).
 """
 
 
@@ -30,7 +30,7 @@ class Grid:
             return True
         return False
     
-    
+
     def move_in_grid(self, vehicle_name, direction, vehicle_size):
         """
         This method checks if a specific move is possible inside the grid. It changes the position of the vehicle
--- code/classes/solution.py
+++ code/classes/solution.py
@@ -1,17 +1,19 @@
-#-----------------------------------------------------
-# solution.py
-#
-# Programmeertheorie
-# Vluchtstrook
-#
-# - This file is one of the parts of rushhour
-# - This file defines a class named Solution
-# - A new object of this class has specific attributes
-#-----------------------------------------------------
+"""
+solution.py
+
+Course: programmeertheorie
+Team: vluchtstrook
+
+This file contains a class named Solution. It saves the path to a solution (where each grid in the path is saved as 
+a string) and contains a counter for all the states that are created while finding a solution, as well as a counter for 
+each unique state that is created while finding a solution.
+An instance of the Solution class is created in the algorithm files (code/algorithms).
+"""
+
 
 class Solution:
 
     def __init__(self):
         self.path = []
-        self.count_unique_states = 0
         self.count_states = 0
+        self.count_unique_states = 0
