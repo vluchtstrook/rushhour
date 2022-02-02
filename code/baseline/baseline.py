6dda137049ec88b55926b8813ea62235f8426a4c
tjardaleppers <t.leppers@outlook.com>
2022-02-02 20:17:39 +0100

    Cleaned up comments, added description to baseline.py and removed redundant plots

--- code/baseline/baseline.py
+++ code/baseline/baseline.py
@@ -1,3 +1,16 @@
+"""
+baseline.py
+
+Course: programmeertheorie
+Team: vluchtstrook
+
+This file contains all code used to determine our baseline. It does not contain a function, but rather consecutive code
+that runs our random algorithm and our first algorithm (breadth first) for a number of times and generates a barplot
+with the results.
+"""
+
+
+
 from fileinput import filename
 from code.algorithms.breadth_first import BreadthFirst
 from code.algorithms.depth_first import DepthFirst
@@ -7,13 +20,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+
 filename = 'data/Rushhour6x6_1.csv'
 
 random_solutions = []
 buckets = []
 nr_solutions_per_bucket = []
 
-for i in range(1000):
+for _ in range(1000):
     rushhour = RushHour(filename)
     random_solution = randomise.random_algo(rushhour)
     random_solutions.append(random_solution.count_unique_states)
@@ -24,22 +38,16 @@ for i in range(1000):
 #     breadth_first_solution = breadth_first_class.run()
 #     random_solutions.append(breadth_first_solution.count_states)
 
-# for i in range(1000):
-#     rushhour = RushHour(filename)
-#     depth_first_class = DepthFirst(rushhour)
-#     depth_first_solution = depth_first_class.run()
-#     random_solutions.append(depth_first_solution.count_states)
-
 max_solution_count = max(random_solutions)
 
 nr_of_buckets = math.ceil(max_solution_count/1000)
 
 for i in range(nr_of_buckets):
-    count = 0
-    for j in range(len(random_solutions)):
-        if random_solutions[j] >= i*1000 and random_solutions[j] < (i+1)*1000:
-            count += 1
-    
+    count = sum(
+        random_solution_ >= i * 1000 and random_solution_ < (i + 1) * 1000
+        for random_solution_ in random_solutions
+    )
+
     nr_solutions_per_bucket.append(count)
     buckets.append(i*1000)
 
