3daaa42873d64bf1f693050845f4bdf15539bffb
tjardaleppers <t.leppers@outlook.com>
2022-02-02 11:55:40 +0100

    Cleaned up comments/description of vehicle.py

--- code/classes/vehicle.py
+++ code/classes/vehicle.py
@@ -1,14 +1,14 @@
-#-----------------------------------------------------
-# vehicle.py
-#
-# Programmeertheorie
-# Vluchtstrook
-#
-# - This file is one of the parts of rushhour
-# - This file defines a class named Vehicle
-# - A new object of this class has specific attributes
-# - It has different methods to return the object's attribute
-#-----------------------------------------------------
+"""
+vehicle.py
+
+Course: programmeertheorie
+Team: vluchtstrook
+
+This file contains a class named Vehicle. It defines two methods that (1) return the orientation of the vehicle and (2) return
+the lenght of the vehicle.
+An instance of the Vehicle class is created in the loader file (loader.py).
+"""
+
 
 class Vehicle():
     
@@ -21,8 +21,14 @@ class Vehicle():
         return self.car
     
     def orientation(self):
+        """
+        This method returns the orientation of the vehicle. This can be either horizontal or vertical.
+        """
         return self.orientation
     
     def length(self):
+        """
+        This method returns the length of the vehicle. This can be either 2 (car) or 3 (truck).
+        """
         return self.length
 
